e053c79ec64c44c35508ce3b1cb08758dcdc92e9
Jawed Alam <zjaweds@outlook.com>
2021-05-16 14:14:34 +0530

    Basic Demonstration of inheritance

---
+++ CheatSheet/InheritanceDemo.java
@@ -0,0 +1,34 @@
+class Building{
+    int numberOfRooms;
+    int numberOfLadders;
+    int numberOfElevators;
+    int numberOfPassages;
+    int numberOfFloors;
+    double surfaceArea; //View from directly above the buliding
+    double buildHeight;
+    public Building(){
+
+    }
+}
+
+class SeminarHall extends Building{
+    public SeminarHall(){
+        this.numberOfFloors=0;
+    }
+    public void findAttributes(){
+        System.out.println(this.numberOfFloors+" floors.");
+        System.out.println(this.numberOfRooms+" rooms.");
+        System.out.println(this.numberOfElevators+" elevators.");
+        System.out.println(this.numberOfPassages+" passages.");
+        System.out.println(this.numberOfLadders+" ladders.");
+    }
+}
+ class InheritanceDemo{
+    public static void main(String a[]){
+        SeminarHall seminarHall1 = new SeminarHall();
+        seminarHall1.numberOfRooms=10;
+        System.out.println("I'm from the seminar Hall.");
+        System.out.println("My specifications:--- ");
+        seminarHall1.findAttributes();
+    }
+ }
