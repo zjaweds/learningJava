22cf1217ac3f4feb9d1c61a3c298f8d2b3ac1f2c
Jawed Alam <zjaweds@outlook.com>
2021-05-30 16:04:47 +0530

    String demonstration further updated

--- Revision/Strings/StringDemo.java
+++ Revision/Strings/StringDemo.java
@@ -1,6 +1,16 @@
+import java.util.*;
+
 class StringObjects{
+    String str;
     public StringObjects(){
         System.out.println("String objetcs class object instantiated");
+    }
+    public String Append(String x){
+        System.out.println(str+x);
+        return str+x;
+    }
+    public void display(){
+        System.out.println(this.str);
     }
 }
 
@@ -13,8 +23,17 @@ class StringObjects{
     public static void main(String [] a){
         System.out.println("Hello");
         StringObjects beta= new StringObjects();
+        String gama = beta.Append("California"); 
+        beta.Append("California");//It demonstarte that strings are immutable
         System.out.println(alpha+" Length of alpha is: "+alpha.length());
         System.out.println("Char at 4th index is: "+ alpha.charAt(4));
-        System.out.println(beta);
+        char[] delta= new char[100];
+        alpha.getChars(0,10,delta,2);
+        beta.display();
+        System.out.println("Gama: "+gama);
+        System.out.print("Delta: ");
+        System.out.println(delta);
+        System.out.println(alpha.equals(gama));
+        System.out.println(alpha==gama);
     }    
 }
